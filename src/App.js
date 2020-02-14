ee1fddefa63d5baf34e12c3cfef13d3069fe5294
Checho <checho9999@gmail.com>
2020-02-14 17:29:37 -0300

    Consumiendo desde mi app principal un registro desde una API publica de forma basica a traves de un fetch, utilizando async y await, y styled components para darle estilo a la app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`
 
 function App() {
+
+/*  const consultarAPI = () => {
+    // Ejemplo sin async y await  
+    //console.log('Consultando la API...');    
+    const api = fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+    const frase = api.then( respuesta => respuesta.json() )
+    frase.then( resultado => console.log(resultado))
+    }*/
+
+  // async y await para que se detenga la ejecucion del codigo hasta que se resuelva api  
+  const consultarAPI = async () => {
+    //console.log('Consultando la API...');    
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    console.log(frase[0]); //Solo me interesa la frase que retorna en el array[0]  
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Boton
+        //onClick={ () => consultarAPI() } // espera que el usuario de click
+        //onClick={consultarAPI()} // no espera que el usuario de click, se ejecuta la funcion JS sin esperar el click
+        onClick={consultarAPI} // espera que el usuario de click
+      >Breaking Bad</Boton>  
+    </Contenedor>    
   );
 }
 
